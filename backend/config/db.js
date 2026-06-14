7238cd1ffb2c46e7eb302114b1fcaf0287568b96
That1neweeb <sidhant.giri32@gmail.cpm>
2026-06-14 11:56:31 +0545

    db.js uses env now

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,20 +1,24 @@
-import {Sequelize} from 'sequelize'
+import dotenv from 'dotenv';
+import { Sequelize } from 'sequelize';
 
-export const sequelize = new Sequelize (
-    'ArgueArena', //database
-    'postgres', //username
-    'root' //password
-, {
-    host : 'localhost',
-    dialect : 'postgres'
+dotenv.config();
+
+const database = process.env.DB_NAME;
+const username = process.env.DB_USER;
+const password = process.env.DB_PASSWORD;
+const host = process.env.DB_HOST || 'localhost';
+const dialect = process.env.DB_DIALECT || 'postgres';
+
+export const sequelize = new Sequelize(database, username, password, {
+  host,
+  dialect,
 });
 
-export const connection = async() => {
-    try{
-        await sequelize.sync({alter:true});
-        console.log("Database connection successful");
-    }
-    catch(e){
-        console.log("Database connection error",e);
-    }
-}
+export const connection = async () => {
+  try {
+    await sequelize.sync({ alter: true });
+    console.log('Database connection successful');
+  } catch (e) {
+    console.log('Database connection error', e);
+  }
+};
